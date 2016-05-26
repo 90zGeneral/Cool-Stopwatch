a26936e8472cc2dcab81e269ca458a47bd44fa6d
Roydon Jeffrey <brushdam14823@gmail.com>
2016-05-26 14:13:47 -0400

    Integrated minutes, seconds, and milliseconds into a timer app

--- Cool Stopwatch/ViewController.swift
+++ Cool Stopwatch/ViewController.swift
@@ -9,6 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    var startTime = NSTimeInterval()
+    
+    @IBOutlet var displayTimeLabel: UILabel!
+    
+    @IBAction func start(sender: AnyObject) {
+        
+        if !timer.valid {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(ViewController.updateTime), userInfo: nil, repeats: true)
+            
+            startTime = NSDate.timeIntervalSinceReferenceDate()
+        }
+        
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        
+        timer.invalidate()
+    }
+    
+    func updateTime() {
+        
+        let currentTime = NSDate.timeIntervalSinceReferenceDate()
+        var elapsedTime: NSTimeInterval = currentTime - startTime
+        
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        let fraction = UInt8(elapsedTime * 100)
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        let strFraction = String(format: "%02d", fraction)
+            
+        displayTimeLabel.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,4 +63,8 @@ class ViewController: UIViewController {
 
 
 }
+
+
+
+
 
